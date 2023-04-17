7048ce8e25fc646681753a97607faa25703f63fc
Ana Martínez <anamartinez.informatica@iespacomolla.es>
2023-04-17 11:21:24 +0200

    Test con fallo

--- calculator.test.js
+++ calculator.test.js
@@ -1,11 +1,11 @@
 const mathOperations = require('./calculator'); 
 
 describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
+    test('adding 1 + 2 should return 10', () => {
       // arrange and act
       var result = mathOperations.sum(1,2)
     
       // assert
-      expect(result).toBe(3);
+      expect(result).toBe(10);
     });
    })
